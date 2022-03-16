ef09852bd1ef59008c343b783af1f62a682b9188
Ivan-TTT <101339844+Ivan-TTT@users.noreply.github.com>
2022-03-16 15:43:14 +0700

    В процессе изменения HTML и CSS
    Хотел установить Panda дл scss и подумал почему бы не установить в основную папку там где у меня проект, я думал программа создаст свой каталог, но нет она к чертям собачим все удалила включая все папки и проекты , благо у меня все было открыто в vs code я сохранил файлы с помледними изменениями и установил с гитхаба снова свой проект. Намучался конкретно но потихоньку пишу основной код страницы чтоб взуальная часть радовала и довала потенциал для дальнейше логики

--- web/JS/mainJquery.js
+++ web/JS/mainJquery.js
@@ -6,6 +6,20 @@ $(document).ready(function() {
     console.log('HTML загружен');
 });
 
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box");
+
+
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
+
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
 function AddNewDopTaskFunction(id) {
     var toggleElements = document.querySelectorAll(".itemGroupTwo");
     var toogledElement = document.querySelector("#" + id);
