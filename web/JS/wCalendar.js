937a7ef399993a256149e415c5e218236523df44
Ivan-TTT <101339844+Ivan-TTT@users.noreply.github.com>
2022-04-10 00:26:31 +0700

    календарь

--- web/JS/wCalendar.js
+++ web/JS/wCalendar.js
@@ -8,13 +8,12 @@ $(document).ready(function() {
     // visibleNum();
     // activeTask();
     // // startW();
-    getAddСalendar();
+    getAddСalendar('0');
 });
 
-const СalendarElement = document.querySelector('.everyDay_');
+const СalendarElement = document.querySelector('.everyDay');
 
-function getAddСalendar(){
-
+function getAddСalendar(nNum){
 
     // Сегодняшняя дата 
     var fullNowDate = new Date();
@@ -34,10 +33,11 @@ function getAddСalendar(){
     var fullDate = new Date();
     // console.log(fullDate);
     // первое этого число месяцы
-    var startDate = new Date(fullDate.setDate(fullDate.getDate() - (NowNumber - 1)));
-    // console.log(startDate);
+    // var startDate = new Date(fullDate.setDate(fullDate.getDate() - (NowNumber + 1)));
+    var startDate = new Date(fullDate.setDate(1));
+    console.log(startDate);
     // День недели первого числа месяца
-    var startDateDay = startDate.getDay();
+    // var startDateDay = startDate.getDay();
     // console.log(startDateDay);
     // --------------------------------------------
 
@@ -45,65 +45,95 @@ function getAddСalendar(){
     var fullDate_e = new Date();
     // console.log(fullDate);
     // первое этого число месяцы
-    var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - (NowNumber - 1)));
-    // console.log(startDate);
+    var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - (NowNumber + 1)));
     // --------------------------------------------
+    console.log(startDate);
+    if (nNum =='1'){
+        // var startDate = new Date(startDate.setDate(1));
+        var startDate = new Date(fullDate.setMonth(startDate.getMonth() - 01));
+        var startDate = new Date(fullDate.setDate(1));
+        // var startDate = new Date(fullDate.setDate(fullDate.getDate() + 1));
+        console.log(startDate);
 
+        // var sssttt = startDate.getMonth() - 1
+        // var startDate = new Date(fullDate.setDate(fullDate.setMonth(sssttt)));
+        // console.log(startDate.toLocaleDateString());
+        $(".e_d").remove();
+    } else if (nNum =='2'){
+        var startDate = new Date(fullDate.setMonth(startDate.getMonth() + 01));
+        var startDate = new Date(fullDate.setDate(1));
+        // var startDate = new Date(fullDate.setDate(fullDate.getDate() + 1));
+        console.log(startDate);
+
+        // // var startDate = new Date(fullDate.setDate(fullDate.getDate() + 41));
+        // // var startDate = new Date(fullDate.setDate(1));
+        // // console.log(startDate);
+        // var startDate = new Date(fullDate.setMonth(startDate.getMonth() + 01));
+        // var startDate = new Date(fullDate.setDate(1));
+        // // var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - 1));
+        $(".e_d").remove();
+    }
+
+    var startDateDay = startDate.getDay();
+    console.log(startDateDay);
     // находим первое число для сетки 7 на 6
     if (startDateDay == '1'){
         console.log(startDateDay);
     } else if (startDateDay == '2'){
         var startDate = new Date(fullDate.setDate(fullDate.getDate() - 1));
         var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - 1));
+        console.log(2);
     } else if (startDateDay == '3'){
         var startDate = new Date(fullDate.setDate(fullDate.getDate() - 2));
-        var endtDate = new Date(ffullDate_e.setDate(fullDate_e.getDate() - 2));
+        var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - 2));
+        console.log(3);
     } else if (startDateDay == '4'){
         var startDate = new Date(fullDate.setDate(fullDate.getDate() - 3));
         var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - 3));
+        console.log(4);
     } else if (startDateDay == '5'){
         var startDate = new Date(fullDate.setDate(fullDate.getDate() - 4));
         var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - 4));
+        console.log(5);
     } else if (startDateDay == '6'){
         var startDate = new Date(fullDate.setDate(fullDate.getDate() - 5));
         var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - 5));
-    } else if (startDateDay == '7'){
+        console.log(6);
+    } else if (startDateDay == '0'){
         var startDate = new Date(fullDate.setDate(fullDate.getDate() - 6));
         var endtDate = new Date(fullDate_e.setDate(fullDate_e.getDate() - 6));
+        console.log(7);
     }
     // console.log(startDate);
 
     // Последнее число для сетки 7 на 6
     var endtDate = new Date(endtDate.setDate(endtDate.getDate() + 41));
 
-    console.log(startDate);
-    console.log(fullNowDate);
-    console.log(endtDate);
 
-    // var startDate = new Date(fullDate.setDate(fullDate.getDate() + 41));
-    // console.log(startDate);
+    console.log(startDate.toLocaleDateString());
+    console.log(fullNowDate.toLocaleDateString());
+    console.log(endtDate.toLocaleDateString());
 
 
+    $(".e_d").remove();
+    for (var date = 0; date <= 41;  date++){
+        const NewСalendarElement = document.createElement ( 'li' );
+        NewСalendarElement.classList.add ( "everyDay" );
+        NewСalendarElement.classList.add ( "e_d" );
+        startDate.getDate()  == NowNumber ? NewСalendarElement.classList.add ( "toDay" ) : "";
+        startDate.getMonth() == NowMonth  ? NewСalendarElement.classList.add ( "toMonth" ) : "";
+        NewСalendarElement.setAttribute ('li_all', `${startDate.toLocaleDateString()}`);
+        NewСalendarElement.setAttribute ('li_Year', `${startDate.getFullYear()}`);
+        NewСalendarElement.setAttribute ('li_Month', `${startDate.getMonth()}`);
+        NewСalendarElement.setAttribute ('li_Date', `${startDate.getDate()}`);
+        NewСalendarElement.innerHTML = `
+            ${startDate.getDate()}
+        `
+        СalendarElement.before(NewСalendarElement);
+        var startDate = new Date(fullDate.setDate(fullDate.getDate() + 1));
 
+    }
 
-    // for (var date = NowNumber; date != 0; date-1 ){
-    //     var startDate = new Date(fullNowDate.setDate(fullNowDate.getDate() - ));
-    //     console.log(startDate);
-    // }
-
-    
-
-    // [...document.getElementsByClassName('fixetT')].forEach(i => i.classList.add("invi"));
-
-    // localStorage.setItem('contact_Created','yes');
-    // localStorage.setItem('contact_Editing','no');
-
-    // const NewСalendarElement = document.createElement ( 'li' );
-    // NewСalendarElement.classList.add ( "everyDay" );
-    // // NewContactElement.setAttribute ('id', '0');
-    // NewСalendarElement.innerHTML = `
-        
-    // `
-    // СalendarElement.after(NewСalendarElement);
+    return
     
 }
